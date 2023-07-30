bef589a002bf6e453d69bd647758aec7a6a0925d
Aditya <aditya.goyat023@gmail.com>
2023-07-29 19:48:02 -0700

    Initial git commit

---
+++ src/main/java/com/mynotesapp/server/mynotesappserver/MynotesappserverApplication.java
@@ -0,0 +1,13 @@
+package com.mynotesapp.server.mynotesappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynotesappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynotesappserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynotesapp/server/mynotesappserver/MynotesappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynotesapp.server.mynotesappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynotesappserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
